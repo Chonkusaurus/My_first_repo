9da6473813d3b993eebd831ace99d83f612366f0
Ria Thampy <riathampy6@gmail.com>
2025-07-04 12:30:08 +0100

    EXTRA UPDATE

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-
-print("Hello Chonkus!")
-eep = input("is the chonkus eepy y/n?  ")
-i=0
-if eep == "y":
-    print("that would be correct")
-elif eep =="n":
-    for i in range(3):
-        print("EEPY TIME!!")
-else:
-        print("I said y/n >:(")
+redo = True
+while redo == True:
+    print("Hello Chonkus!")
+    eep = input("is the chonkus eepy y/n?  ")
+    i=0
+    if eep == "y":
+        print("that would be correct")
+    elif eep =="n":
+        for i in range(3):
+            print("EEPY TIME!!")
+    else:
+            
+            print("I said y/n >:(")
+            redo == True
