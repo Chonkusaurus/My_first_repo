c19c266aa6a910ef6cc0684b8748fa76e25404c0
Ria Thampy <riathampy6@gmail.com>
2025-07-04 12:22:59 +0100

    Eep commit

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+
 print("Hello Chonkus!")
+eep = input("is the chonkus eepy y/n?  ")
+i=0
+if eep == "y":
+    print("that would be correct")
+elif eep =="n":
+    for i in range(3):
+        print("EEPY TIME!!")
+else:
+        print("I said y/n >:(")
